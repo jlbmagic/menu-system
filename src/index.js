02a689be8de277ba88e05e7186714d9a62a9040d
integrating-magic <jeremy@integratingmagic.io>
2024-09-19 11:30:55 -0400

    added orientation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 window.loadMenu = (json) => {
   const obj = JSON.parse(json);
   console.log(obj);
-  const menuItems = obj.data;
+  const { data: menuItems, orientation = "horizontal" } = obj; // Extract menu items and orientation
   const menu = document.getElementById("menu");
 
   // Clear previous menu items if any
   menu.innerHTML = "";
+
+  // Add orientation class to menu container
+  if (orientation === "horizontal") {
+    menu.classList.add("menu-horizontal");
+    menu.classList.remove("menu-vertical");
+  } else {
+    menu.classList.add("menu-vertical");
+    menu.classList.remove("menu-horizontal");
+  }
 
   menuItems.forEach((item) => {
     const menuItem = document.createElement("li");
